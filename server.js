ef46b7338eb1d24cfefe219005242a1e48ef153b
rushi725 <rushabh725@gmail.com>
2017-02-09 19:58:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/Rushabh', function (req, res) {
-  res.send('My name is Rushabh');
+  res.sendFile(path.join(__dirname, 'ui', 'Rushabh.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
