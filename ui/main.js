7eadb717d3261a118ce6cf55fa2fcc4e53817fb0
rushi725 <rushabh725@gmail.com>
2017-02-28 19:13:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var wall = document.getElementById('mwall');
 wall.onClick = function(){
     var interval = setInterval(moveRight,100);
-}
+};
 var marginLeft = 0;
 function moveRight(){
     marginLeft=marginLeft+10;
