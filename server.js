50c6f8b1e0b580bab6db9bdb104884c4decc9e20
rushi725 <rushabh725@gmail.com>
2017-03-16 18:28:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database:'rushi725',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.emv.DB_PASSWORD
+    password:process.env.DB_PASSWORD
 };
 
 var app = express();
