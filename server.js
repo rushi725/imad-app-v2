3e770b9d8de2445effdab96004fa5a282f894dcd
rushi725 <rushabh725@gmail.com>
2017-03-17 18:57:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 /*var pool = require('pg').pool;
 var config = {
     user: 'rushi725',
@@ -31,6 +32,17 @@ app.get('/test-db',function (req,res){
         }
     });
 });*/
+
+function hash (input){
+    //how do we create a hash
+    var hashhed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashhed.toString('hex');
+    
+}
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash(req.params.input, 'this is some random string');
+    res.send(hashedString);
+})
 
 app.get('/ui/sidebar.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sidebar.css'));
